71e868fbbc4fb7144ac4e8f8ef872176d9a9986b
H0dis <147428577+H0dis@users.noreply.github.com>
2025-05-31 00:11:47 +0300

    Aplicatie functionala

--- app/src/main/java/com/example/vinylrecordfinder/data/remote/DiscogsApi.kt
+++ app/src/main/java/com/example/vinylrecordfinder/data/remote/DiscogsApi.kt
@@ -1,14 +1,14 @@
 package com.example.vinylrecordfinder.data.remote
 
-
 import retrofit2.http.GET
 import retrofit2.http.Query
+import kotlinx.serialization.Serializable
 
 // Model de răspuns
 data class DiscogsResponse(
     val results: List<DiscogsAlbum>
 )
-
+@Serializable
 data class DiscogsAlbum(
     val title: String,
     val thumb: String,
